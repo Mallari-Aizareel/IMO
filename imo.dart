7263515fee6101fea26048bbdc17d161272489c0
Cecilia <144504923+ceciliasds@users.noreply.github.com>
2024-02-23 11:45:16 +0800

    Delos Santos, Cecilia
    added appbar

--- imo.dart
+++ imo.dart
@@ -36,3 +36,30 @@ class ChatScreenState extends State<ChatScreen> with SingleTickerProviderStateMi
     super.dispose();
   }
 }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: AppBar(
+          automaticallyImplyLeading: false,
+          title: null,
+          bottom: TabBar(
+            controller: _tabController,
+            tabs: [
+              Tab(
+                icon: CircleAvatar(
+                  radius: 12.0,
+                  backgroundImage: NetworkImage(
+                    'https://i.pinimg.com/originals/d0/39/66/d039663178d22cc2a63170e569787e9f.jpg',
+                  ),
+                ),
+              ),
+              Tab(icon: Icon(Icons.chat_rounded)),
+              Tab(icon: Icon(Icons.record_voice_over)),
+              Tab(icon: Icon(Icons.person_add_alt_1)),
+            ],
+          ),
+        ),
+      ),
